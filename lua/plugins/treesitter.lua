a8862aaaa1b2d40ee67a84d033871f5ad508ae62
Sean Alter <seanalter1@gmail.com>
2026-01-13 16:08:50 -0500

    Treesitter still broken

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,87 +1,77 @@
-return {
-  -- Main treesitter plugin
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    main = "nvim-treesitter.configs",
-    dependencies = {
-      {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-        enabled = false,
-        config = function()
-          local move = require("nvim-treesitter.textobjects.move")
-          local configs = require("nvim-treesitter.configs")
-          for name, fn in pairs(move) do
-            if name:find("goto") == 1 then
-              move[name] = function(q, ...)
-                if vim.wo.diff then
-                  local config = configs.get_module("textobjects.move")[name]
-                  for key, query in pairs(config or {}) do
-                    if q == query and key:find("[%]%[][cC]") then
-                      vim.cmd("normal! " .. key)
-                      return
-                    end
-                  end
-                end
-                return fn(q, ...)
-              end
-            end
-          end
-        end,
-      },
-    },
-    opts = {
-      ensure_installed = {
-        "lua",
-        "vim",
-        "vimdoc",
-        "markdown",
-        "markdown_inline",
-        "bash",
-        "c",
-        "html",
-        "css",
-        "javascript",
-        "typescript",
-        "json",
-        "yaml",
-        "toml",
-        "python",
-        "java",
-        "go",
-        "sql",
-        "dockerfile",
-        "gitignore",
-        "make",
-        "cmake",
-        "regex",
-      },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        use_languagetree = true,
-        additional_vim_regex_highlighting = { "ruby" },
-      },
-      indent = { enable = true },
-    },
-    config = function(_, opts)
-      -- Safer loading with error handling
-      local ok, treesitter = pcall(require, "nvim-treesitter.configs")
-      if not ok then
-        vim.notify("Treesitter failed to load", vim.log.levels.ERROR)
-        return
-      end
-      treesitter.setup(opts)
-    end,
-  },
+return { -- Highlight, edit, and navigate code
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	main = "nvim-treesitter.configs", -- Sets main module to use for opts
+	-- Autoinstall languages that are not installed
+	dependencies = {
+		{
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			enabled = false,
+		},
+	},
+	opts = {
+		ensure_installed = {
+			"lua",
+			"python",
+			"javascript",
+			"typescript",
+			"vimdoc",
+			"vim",
+			"regex",
+			"sql",
+			"dockerfile",
+			"toml",
+			"json",
+			"java",
+			"go",
+			"gitignore",
+			"c", -- Replaced "clang" with "c"
+			"cpp", -- Added cpp usually good to have with c
+			"yaml",
+			"make", -- Covers Makefiles (Removed "Makefile")
+			"cmake",
+			"markdown",
+			"markdown_inline",
+			"bash",
+			"css",
+			"html",
+		},
+		auto_install = true,
+		highlight = {
+			enable = true,
+			use_languagetree = true,
+			-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+			--  If you are experiencing weird indenting issues, add the language to
+			--  the list of additional_vim_regex_highlighting and disabled languages for indent.
+			additional_vim_regex_highlighting = { "ruby" },
+		},
+		indent = { enable = true },
+	},
+	config = function(_, opts)
+		require("nvim-treesitter.configs").setup(opts)
 
-  -- Treesitter context (separate plugin)
-  {
-    "nvim-treesitter/nvim-treesitter-context",
-    enabled = true,
-    opts = {
-      mode = "cursor",
-      max_lines = 3,
-    },
-  },
+		local move = require("nvim-treesitter.textobjects.move") ---@type table<string,fun(...)>
+		local configs = require("nvim-treesitter.configs")
+		for name, fn in pairs(move) do
+			if name:find("goto") == 1 then
+				move[name] = function(q, ...)
+					if vim.wo.diff then
+						local config = configs.get_module("textobjects.move")[name] ---@type table<string,string>
+						for key, query in pairs(config or {}) do
+							if q == query and key:find("[%]%[][cC]") then
+								vim.cmd("normal! " .. key)
+								return
+							end
+						end
+					end
+					return fn(q, ...)
+				end
+			end
+		end
+	end,
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		enabled = true,
+		opts = { mode = "cursor", max_lines = 3 },
+	},
 }
