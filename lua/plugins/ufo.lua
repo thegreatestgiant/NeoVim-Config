45db5da27fe12259500625e454b8677251eb9594
Sean Alter <seanalter1@gmail.com>
2026-01-14 23:17:25 -0500

    "

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -3,12 +3,6 @@ return {
 	dependencies = { "kevinhwang91/promise-async" },
 	opts = {}, -- Empty options, or add your specific settings
 	config = function()
-		-- Recommended settings from the nvim-ufo README for LSP or Treesitter folding
-		vim.o.foldcolumn = "1"
-		vim.o.foldlevel = 99
-		vim.o.foldlevelstart = 99
-		vim.o.foldenable = true
-
 		require("ufo").setup({
 			provider_selector = function(bufnr, filetype, buftype)
 				-- Fallback to the 'indent' provider if treesitter isn't available
