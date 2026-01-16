f17f031519a53b2905f7cc03129914a40528b264
Sean Alter <seanalter1@gmail.com>
2026-01-15 21:56:24 -0500

    🐛 Fix: Resolve keymap conflict for terminal toggle
    Removed the <C-\> mapping from vim-tmux-navigator to allow toggleterm to
    bind it for opening the floating terminal.

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -15,7 +15,6 @@ return {
 			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>", desc = "Navigate down" },
 			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>", desc = "Navigate up" },
 			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>", desc = "Navigate right" },
-			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>", desc = "Navigate previous" },
 		},
 	},
 	{
